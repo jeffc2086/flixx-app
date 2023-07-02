b8857517d29950bfe500f6041194aa5f1e601905
jeffc2086 <136286623+jeffc2086@users.noreply.github.com>
2023-07-01 19:45:06 -0500

    Fix TV names

--- js/script.js
+++ js/script.js
@@ -64,12 +64,12 @@ async function displayPopularShows() {
             `<img
             src="images/no-image.jpg"
             class="card-img-top"
-            alt="${show.orignal_name}"
+            alt="${show.name}"
           />`
         }
         </a>
         <div class="card-body">
-          <h5 class="card-title">${show.orignal_name}</h5>
+          <h5 class="card-title">${show.name}</h5>
           <p class="card-text">
             <small class="text-muted">Release: ${show.first_air_date}</small>
           </p>
